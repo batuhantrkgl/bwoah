76208dc2d8c2b4eba5d22238b6569e4b5fc33c0f
Batuhantrkgl <batuhanturkoglu37@gmail.com>
2024-06-06 22:11:01 +0300

    feat: Update presence status and activity on client ready event
    - Set the client's activity to display the number of days left to LeMans.
    - Set the client's status to idle.

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,11 @@
-const { Events } = require('discord.js');
+const { Events, PresenceUpdateStatus } = require('discord.js');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
+		client.user.setActivity(`${Math.ceil((new Date('2024-06-15') - new Date()) / (1000 * 60 * 60 * 24))} days Left to LeMans!`);
+		client.user.setStatus(PresenceUpdateStatus.Idle);
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
